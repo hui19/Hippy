230e350721b1fae3017d66c86d5296576d2a2e7f
maxli <maxli@tencent.com>
2020-04-27 16:47:46 +0800

    refactor(android-viola): remove so loader from sdk

--- android/sdk/src/main/java/com/tencent/mtt/hippy/bridge/libraryloader/LibraryLoader.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/bridge/libraryloader/LibraryLoader.java
@@ -48,7 +48,6 @@ public class LibraryLoader
 	 * Size of reading buffers.
 	 */
 	private static final int				BUFFER_SIZE					= 0x4000;
-	private static Context					sContext;
 	private static SharedPreferences		sSharedPreferences;
 	private static String					DEFAULT_LIBRARY_DIR;
 	private static String					PRIVATE_LIBRARY_DIR;
@@ -58,10 +57,10 @@ public class LibraryLoader
 	static
 	{
 		SO_LIST = new ArrayList<String>();
-		SO_LIST.add("libmtt_shared.so");
-		SO_LIST.add("libmttv8.so");
-		SO_LIST.add("libhippybridge.so");
-		SO_LIST.add("libflexbox.so");
+//		SO_LIST.add("libmtt_shared.so");
+//		SO_LIST.add("libmttv8.so");
+//		SO_LIST.add("libhippybridge.so");
+//		SO_LIST.add("libflexbox.so");
 	}
 
 	public static void init(Context context,  SharedPreferences sharedPreferences,
@@ -69,7 +68,6 @@ public class LibraryLoader
 	{
 		try
 		{
-			sContext = context;
 			sSharedPreferences = sharedPreferences;
 			sHippySoLoaderAdapter = hippySoLoaderAdapter;
 			ApplicationInfo applicationInfo = context.getApplicationInfo();
@@ -86,7 +84,7 @@ public class LibraryLoader
 
 		String libraryName = mapLibraryName(shortName);
 		boolean isHippySo = SO_LIST.contains(libraryName);
-		if (isHippySo && SO_LOADED_LIST.contains(libraryName))
+		if (true || isHippySo && SO_LOADED_LIST.contains(libraryName))
 		{
 			return;
 		}
@@ -192,284 +190,9 @@ public class LibraryLoader
 		return false;
 	}
 
-	private static boolean loadFormPrivateDir(String libraryName)
-	{
-		if (TextUtils.isEmpty(PRIVATE_LIBRARY_DIR))
-		{
-			return false;
-		}
-		try
-		{
-			if (checkPrivateLibrary(libraryName))
-			{
-
-				File file = new File(PRIVATE_LIBRARY_DIR, libraryName);
-				System.load(file.getAbsolutePath());
-				return true;
-			}
-			else
-			{
-				//这里从原始apk中解压一份
-				ApplicationInfo info = sContext.getApplicationInfo();
-				if (info == null)
-				{
-					return false;
-				}
-				File sourceApk = new File(info.sourceDir);
-				if (sourceApk == null || !sourceApk.exists())
-				{
-					return false;
-				}
-
-				File targetFile = new File(PRIVATE_LIBRARY_DIR, libraryName);
-				// 先删除老文件
-				if (targetFile != null && targetFile.exists())
-				{
-					targetFile.delete();
-				}
-				if (!targetFile.exists())
-				{
-					if(targetFile.getParentFile() != null)
-					{
-						targetFile.getParentFile().mkdirs();
-					}
-					targetFile.createNewFile();
-				}
-
-
-				//这里做一次改造，先走ZipFile的模式进行解压，如果不成功，再走一次ZipInputStream（因为ZipFile的模式，要求zip文件必须没有完全没有损坏）
-				try
-				{
-					unZipByZipFile(sourceApk, libraryName, targetFile);
-					// load so here
-					if (targetFile == null || !targetFile.exists())
-					{
-						throw new RuntimeException("unziped file is null");
-					}
-
-					System.load(targetFile.getAbsolutePath());
-				}
-				catch (Throwable e)
-				{
-					unZipByZipInputStream(sourceApk, libraryName, targetFile);
-					// load so here
-					if (targetFile == null || !targetFile.exists())
-					{
-						throw new RuntimeException("unziped file is null");
-					}
-					System.load(targetFile.getAbsolutePath());
-				}
-
-				SharedPreferences.Editor edit = sSharedPreferences.edit();
-				edit.putLong(libraryName + "_crc", getZipCrc(sourceApk));
-				edit.putLong(libraryName + "_timestamp", getTimeStamp(sourceApk));
-				edit.commit();
-
-				return true;
-			}
-		}
-		catch (Throwable e)
-		{
-			SharedPreferences.Editor edit = sSharedPreferences.edit();
-			edit.putLong(libraryName + "_crc", NO_VALUE);
-			edit.putLong(libraryName + "_timestamp", NO_VALUE);
-			edit.commit();
-		}
-		return false;
-	}
-
-	private static boolean checkPrivateLibrary(String libraryName) throws IOException
-	{
-		File file = new File(PRIVATE_LIBRARY_DIR, libraryName);
-		if (file == null || !file.exists())
-		{
-			return false;
-		}
-
-		ApplicationInfo info = sContext.getApplicationInfo();
-		if (info == null)
-		{
-			return false;
-		}
-		File sourceApk = new File(info.sourceDir);
-		if (sourceApk == null || !sourceApk.exists())
-		{
-			return false;
-		}
-
-		long currentCrc = getZipCrc(sourceApk);
-		if (currentCrc == NO_VALUE)
-		{
-			return false;
-		}
-
-		long currentTimeStamp = getTimeStamp(sourceApk);
-		if (currentTimeStamp == NO_VALUE)
-		{
-			return false;
-		}
-
-		long crc = sSharedPreferences.getLong(libraryName + "_crc", NO_VALUE);
-		long timeStamp = sSharedPreferences.getLong(libraryName + "_timestamp", NO_VALUE);
-
-		//这里需要判断下CRC,也需要判断下apk最后更新时间，保证apk的正确性
-		if (crc == currentCrc && currentCrc != NO_VALUE && currentTimeStamp == timeStamp && currentTimeStamp != NO_VALUE)
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	private static void unZipByZipFile(File sourceApk, String libraryName, File targetFile) throws Throwable
-	{
-		ZipFile apk = null;
-		FileOutputStream out = null;
-		InputStream in = null;
-		try
-		{
-			apk = new ZipFile(sourceApk);
-			ZipEntry soFileEntry = apk.getEntry("lib/armeabi/" + libraryName);
-			if (soFileEntry != null)
-			{
-				out = new FileOutputStream(targetFile);
-				in = apk.getInputStream(soFileEntry);
-
-				byte[] buffer = new byte[BUFFER_SIZE];
-				int length = in.read(buffer);
-				while (length != -1)
-				{
-					out.write(buffer, 0, length);
-					length = in.read(buffer);
-				}
-				out.flush();
-			}
-			else
-			{
-				throw new RuntimeException("unZipByZipFile : ZipEntry is null");
-			}
-		}
-		catch (Throwable e)
-		{
-			throw e;
-		}
-		finally
-		{
-			if (apk != null)
-			{
-				try
-				{
-					apk.close();
-				}
-				catch (Throwable e)
-				{
-
-				}
-			}
-			if (in != null)
-			{
-				try
-				{
-					in.close();
-				}
-				catch (Throwable e)
-				{
-				}
-			}
-			if (out != null)
-			{
-				try
-				{
-					out.close();
-				}
-				catch (Throwable e)
-				{
-				}
-			}
-		}
-
-	}
-
-	private static void unZipByZipInputStream(File sourceApk, String libraryName, File targetFile) throws Throwable
-	{
-		FileInputStream fis = new FileInputStream(sourceApk);
-		ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
-		FileOutputStream fos = new FileOutputStream(targetFile);
-		try
-		{
-			ZipEntry entry;
-			byte[] buffer = new byte[BUFFER_SIZE];
-			while ((entry = zis.getNextEntry()) != null)
-			{
-				String entryName = entry.getName();
-				if (TextUtils.isEmpty(entryName) || !entryName.endsWith(".so") || entryName.contains("../"))
-				{// 过滤../异常字符串
-					continue;
-				}
-				int index = entryName.lastIndexOf(File.separator);
-				if (index >= entryName.length() - 2)
-				{
-					continue;
-				}
-				entryName = (index != -1 ? entryName.substring(index + 1) : entryName);
-				if (TextUtils.equals(entryName, libraryName))
-				{
-
-					int count;
-					while ((count = zis.read(buffer)) != -1)
-					{
-						fos.write(buffer, 0, count);
-					}
-					fos.flush();
-					break;
-				}
-			}
-
-		}
-		finally
-		{
-			fos.close();
-			zis.close();
-		}
-	}
-
-	private static long getZipCrc(File archive) throws IOException
-	{
-		long computedValue = ZipUtil.getZipCrc(archive);
-		if (computedValue == NO_VALUE)
-		{
-			computedValue--;
-		}
-		return computedValue;
-	}
-
-	private static long getTimeStamp(File archive)
-	{
-		long timeStamp = archive.lastModified();
-		if (timeStamp == NO_VALUE)
-		{
-			// never return NO_VALUE
-			timeStamp--;
-		}
-		return timeStamp;
-	}
-
-	private static String getPrivateLibraryPath(String libraryName)
-	{
-		if (TextUtils.isEmpty(PRIVATE_LIBRARY_DIR))
-		{
-			return null;
-		}
-		else
-		{
-			File file = new File(PRIVATE_LIBRARY_DIR, libraryName);
-			if (file != null && file.exists())
-			{
-				return file.getAbsolutePath();
-			}
-		}
-		return null;
-	}
+  private static boolean loadFormPrivateDir(String libraryName) {
+	  return false;
+  }
 
 	/**
 	 * Returns the platform specific file name format for the shared library
