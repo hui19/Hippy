11f26e3d98673dce3da0984f0f9646d1ae177cd2
superzheng <superzheng@tencent.com>
2021-04-21 17:45:17 +0800

    refactor(android): Move the native method to the calling class

--- android/sdk/src/main/java/com/tencent/mtt/hippy/bridge/serialization/delegate/DeserializerDelegate.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/bridge/serialization/delegate/DeserializerDelegate.java
@@ -1,11 +1,11 @@
 package com.tencent.mtt.hippy.bridge.serialization.delegate;
 
-import com.tencent.mtt.hippy.NativeAccess;
 import com.tencent.mtt.hippy.runtime.builtins.JSSharedArrayBuffer;
 import com.tencent.mtt.hippy.runtime.builtins.wasm.WasmModule;
 import com.tencent.mtt.hippy.serialization.exception.DataCloneDeserializationException;
 import com.tencent.mtt.hippy.serialization.recommend.Deserializer;
 
+@SuppressWarnings("JavaJniMissingFunction")
 public class DeserializerDelegate implements Deserializer.Delegate {
   private static final DeserializerDelegate instance = new DeserializerDelegate();
 
@@ -24,11 +24,14 @@ public class DeserializerDelegate implements Deserializer.Delegate {
 
   @Override
   public JSSharedArrayBuffer getSharedArrayBufferFromId(Deserializer deserializer, int clone_id) {
-    return NativeAccess.getSharedArrayBufferFromId(clone_id);
+    return getSharedArrayBufferFromId(clone_id);
   }
 
   @Override
   public WasmModule getWasmModuleFromId(Deserializer deserializer, int transfer_id) {
-    return NativeAccess.getWasmModuleFromId(transfer_id);
+    return getWasmModuleFromId(transfer_id);
   }
+
+  public static native JSSharedArrayBuffer getSharedArrayBufferFromId(int clone_id);
+  public static native WasmModule getWasmModuleFromId(int transfer_id);
 }
