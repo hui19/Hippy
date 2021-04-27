e9e0df57fcfbf595fd09295fe6bdf789e7bcfe68
niuniuyang <niuniuyang@tencent.com>
2021-04-27 11:16:23 +0800

    feat: 修改刷新listview，系统的recyclerView 直接滚到顶部到问题

--- android/sdk/src/main/java/com/tencent/mtt/hippy/uimanager/ListItemRenderNode.java
+++ android/sdk/src/main/java/com/tencent/mtt/hippy/uimanager/ListItemRenderNode.java
@@ -15,6 +15,7 @@
  */
 package com.tencent.mtt.hippy.uimanager;
 
+import android.view.View;
 import com.tencent.mtt.hippy.HippyRootView;
 import com.tencent.mtt.hippy.common.HippyMap;
 import com.tencent.mtt.hippy.views.list.IRecycleItemTypeChange;
@@ -45,12 +46,18 @@ public class ListItemRenderNode extends RenderNode
 		}
 	}
 
-
+	/**
+	 * y值是前端传入的，前端没有复用的概念，所有y是整个list长度的y值，并不是recyclerView的排版的y。
+	 * 真正意义上面的y是排版到屏幕范围以内的y，也是子view相对于recyclerView的起始位置的y，也就是子view的top
+	 * 系统的recyclerView在刷新list前，layoutManager会调用anchorInfo.assignFromView，取第一个view计算当前的
+	 * anchorInfo，如果整个地方把y值修改了，导致anchorInfo会取不对.
+	 * 这里保证updateLayout不要改变已经挂在到RecyclerView的view的top
+	 */
 	@Override
-	public void updateLayout(int x, int y, int w, int h)
-	{
+	public void updateLayout(int x, int y, int w, int h) {
 		super.updateLayout(x, y, w, h);
-		this.mY = 0;
+		View renderView = mComponentManager.mControllerRegistry.getView(mId);
+		mY = renderView != null ? renderView.getTop() : 0;
 		if (getParent() != null && mComponentManager != null && mComponentManager.mContext != null)
 		{ // 若屏幕内node更新引起了item整体变化，需要通知ListView发起dispatchLayout重排版
 			RenderManager renderManager = mComponentManager.mContext.getRenderManager();
