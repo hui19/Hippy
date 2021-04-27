42392fa5802953d7a60756cd29597ef0be51b214
niuniuyang <niuniuyang@tencent.com>
2021-04-27 11:49:54 +0800

    fix: 修改错误的注释

--- android/sdk/src/main/java/androidx/recyclerview/widget/EasyCacheExtension.java
+++ android/sdk/src/main/java/androidx/recyclerview/widget/EasyCacheExtension.java
@@ -1,14 +1,13 @@
 package androidx.recyclerview.widget;
 
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.Recycler;
 import android.view.View;
+import androidx.recyclerview.widget.RecyclerView.Recycler;
 
 /**
  * Created by niuniuyang on 2021/1/28.
  * Description
  * 主要用于截获getViewForPositionAndType函数，用于记录当前正在获取view的位置，用于
- * {@link EasyRecyclerPool#getRecycledView(int)} 用来在缓存池里面获取最近合适的ViewHolder
+ * {@link HippyRecyclerPool#getRecycledView(int)} 用来在缓存池里面获取最近合适的ViewHolder
  */
 public class EasyCacheExtension extends RecyclerView.ViewCacheExtension {
 
